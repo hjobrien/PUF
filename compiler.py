f8ab5d931d74b91c6144add3e840c08f656bc4d5
wuisawesome <itswu.alex@gmail.com>
2017-04-09 00:39:26 -0700

    Whitespace parsing done?
    AFAIK this is valid whitespace parsing for python like whitespacing

---
+++ compiler.py
@@ -0,0 +1,48 @@
+from string_helper import getLeadingWhitespace, whitespaceValid
+import re
+
+inFileName = "samplecode.hlf"
+outFileName = "samplecode.py"
+
+# out = open(outFileName, "w")
+# out.write("import library")
+def _main():
+	indentation = [""]
+	prevRelLine = [None] #Previous relevant line
+	startOfBlock = False
+	line_no = 1
+
+	for line in open(inFileName, "r"):
+		if not len(line.strip()):
+			continue
+
+		assert whitespaceValid(line, indentation, startOfBlock), "Error: Invalid whitespace on line: %i" % line_no
+		if startOfBlock:
+			indentation.append(getLeadingWhitespace(line))
+			prevRelLine.append(None)
+		ws = getLeadingWhitespace(line)
+		while indentation and ws != indentation[-1]:
+			indentation.pop()
+			temp = prevRelLine.pop()
+			assert temp == None, "Error, failed to properly close %s" % temp
+
+
+		command = line.split()[0]
+		if prevRelLine[-1] != None and [-1][0] == "Do":
+			0 == 0
+			#TODO: Go back to the latest Do statement and fill in the params of the for loop
+		if prevRelLine[-1] != None:
+			prevRelLine.pop()
+		print(command)
+
+
+		line_no += 1
+		startOfBlock = startsBlock(command)
+		if startOfBlock:
+			prevRelLine.append((command, line_no))
+
+_blockStarters = ["create", "python:", "do", "if", "else", "else if"]
+def startsBlock(command):
+	return command.lower() in _blockStarters
+
+_main()
---
+++ string_helper.py
@@ -0,0 +1,14 @@
+def getLeadingWhitespace(str):
+	length = len(str) - len(str.lstrip())
+	return str[:length]
+
+def whitespaceValid(line, indentation, startOfBlock):
+	ws = getLeadingWhitespace(line)
+	prev = indentation[-1]
+	if startOfBlock:
+		return len(prev) < len(ws) and prev == ws[:len(prev)]
+	else:
+		indentation = indentation[:] #Don't want to modify the original
+		while len(ws) < len(prev) and indentation:
+			prev = indentation.pop()
+		return prev == ws
