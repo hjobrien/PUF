27cd8eb87dcb01b8592ee1dfc5c55e1e2cd25f6b
Hank O'Brien <hank.obrien900@icloud.com>
2017-04-11 20:26:47 -0700

    added IndentManager

--- IndentManager.py
+++ IndentManager.py
@@ -1,53 +1,61 @@
-import re
 
 
 class IndentManager:
 
-    def __init__(self, use_tabs=False):
-        self.__use_tabs = use_tabs
-        self.__curr_indent = 0
-        # keeps a record of the indent level for each level, so if one level can't be both 2 and 4 spaces
-        self.indent_record = [0]
-        self.opens_block = False
-        self.new_block = False
+    def __init__(self):
+        self.indentRecord = [0]
+        self._opensBlock = False
+        self._new_block = False
         self._blockStarters = ["create", "python:", "do", "if", "else", "else if"]
 
-    def get_indent_level(self):
+    def getIndentLevel(self):
         """
         :return: a 0-based indent level of the code currently
         """
-        return len(self.indent_record) - 1
+        return len(self.indentRecord) - 1
 
     def startsBlock(self, command):
         return command.lower() in self._blockStarters
 
-    def check_line(self, line):
+    def checkLine(self, line):
         """
         :param line: raw source code line to test
         :return: true if the indent level is valid, false otherwise
         """
-        self.new_block = self.opens_block
-
-        if self.startsBlock(line):
-            self.opens_block = True
-
-        line = line.replace('\t', '  ')
-        tokens = re.compile('[^\s]+').split(line)
-        indent = len(tokens[0])
-
-        if indent > self.indent_record[-1] and self.new_block:
-            self.indent_record.append(indent)
-            return True
+        if(self._opensBlock):
+            self._new_block = True
+            self._opensBlock = False
         else:
-            temp_record = self.indent_record[:]
-            for i in range(len(self.indent_record)-1, -1, -1):
-                print(self.indent_record)
-                indent_level = self.indent_record[i]
-                if indent_level == indent:
-                    self.__curr_indent = indent_level
-                    self.indent_record = temp_record
-                    return True
-                else:
-                    temp_record.pop()
-            # no previous indent level found, invalid indent
-            return False
+            self._new_block = False
+
+        for keyword in self._blockStarters:
+            if line.index(keyword) != -1:
+                self._opensBlock = True
+
+        whitespace = getLeadingWhitespace(line)
+        if whitespace != self.indentRecord[-1]:
+            if self.isValidIndent(whitespace, self.indentRecord[-1]):
+                self.indentRecord.append(whitespace)
+
+        return whitespaceValid(line, whitespace, self.indentRecord[-1])
+
+
+    def isValidIndent(self, indent, lastIndent):
+        return indent.index(lastIndent) != 0 and self._new_block
+
+
+def getLeadingWhitespace(line):
+    length = len(line) - len(line.lstrip())
+    return line[:length]
+
+
+def whitespaceValid(line, indentation, startOfBlock):
+    ws = getLeadingWhitespace(line)
+    prev = indentation[-1]
+    if startOfBlock:
+        return len(prev) < len(ws) and prev == ws[:len(prev)]
+    else:
+        indentation = indentation[:]  # Don't want to modify the original
+        while len(ws) < len(prev) and indentation:
+            prev = indentation.pop()
+        return prev == ws
